d3999e0fb109495704ab916aa585fb6d21998cb4
Sophie-DIA <sophie@dia-analysis.com>
2020-06-13 16:07:44 -0400

    Lamar

--- src/scribble.py
+++ src/scribble.py
@@ -3,6 +3,8 @@ import numpy as np
 f = 1
 f = 3
 w = 8
-my_k_list = range(0, 10, 2)
+my_k_list = range(0, 8, 2)
 for k in my_k_list:
-    print('Some stuff')
+    print('I got')
+print('loyalty,got royalty')
+
