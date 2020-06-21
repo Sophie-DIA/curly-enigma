336250e0791e5d7d3f0b7baef56d841dd515b8e9
Sophie-DIA <sophie@dia-analysis.com>
2020-06-21 17:07:06 -0400

    Lea image quantizer

--- src/scribble.py
+++ src/scribble.py
@@ -1,12 +1,29 @@
-import numpy as np
-
 f = 1
-f = 3
 w = 8
 my_k_list = range(0, 8, 2)
-for k in my_k_list:
-    print('I got')
-print('loyalty,got royalty')
-print('Inside my DNA')
-print('By')
-print('Kendrik L.')
+my_list = [{'first_key': 'a', 'second_key': '1'},
+           {'first_key': 'c', 'second_key': '2'}]
+
+for item in my_list:
+    print(item['first_key'])
+
+
+class FirstClass:
+    def __init__(self, some_argument):
+        self.my_param = some_argument
+
+    def say_hello(self):
+        print('Hi there! my_param is ' + self.my_param)
+
+
+class BabyClass(FirstClass):
+    def say_hello(self):
+        super().say_hello()
+        print('Just like Papa')
+
+
+my_class = FirstClass('blabla')
+my_class.say_hello()
+
+my_baby_class = BabyClass('aaaap!')
+my_baby_class.say_hello()
